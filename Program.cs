22fc34ab859e9fb5595f33dcc1555d4bab88604b
Yangjun <aleckyj@hotmail.com>
2021-01-04 14:48:46 +0800

    第一次提交

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RxLearning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!哇哈哈");
         }
     }
 }
